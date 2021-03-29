d052b77db2615eb5e06d1c26f3e15847557a6f99
W Lawless <wlawless.sharpe@protonmail.com>
2021-03-29 16:43:01 -0400

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by W Lawless on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
